743459bbb144eaebb7cea9d4a134f34bf8a3e25a
Poetry <jingguanliuye@gmail.com>
2016-05-01 12:53:12 +0800

    Update 字符串strstr-搜索查找.c

--- ReadingBookNotes/HeadFirstC/Demo/字符串strstr-搜索查找.c
+++ ReadingBookNotes/HeadFirstC/Demo/字符串strstr-搜索查找.c
@@ -1,3 +1,19 @@
+/**
+	总结：
+	1、可以用char string[..][..]创建数组的数组
+	2、第一组方括号用来访问外层数组，第二组方括号用来访问内层数组
+	3、有了string.h 可以使用c标准库中的字符串处理函数
+	4、字符串数组是 数组的数组
+	
+        strstr(a,b) 返回字符串b在字符串a中的地址
+	strcpy()复制字符串
+	strcmp()比较字符串
+	strcat()链接字符串
+	strchr用来在字字符串中找到某个字符的位置
+	strlen()获取字符串的长度
+
+*/
+
 #include<stdio.h>
 #include<string>
 
