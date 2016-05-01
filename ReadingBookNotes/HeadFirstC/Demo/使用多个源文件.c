9e063386b53b411f75dc88937afef2b35c74bd38
Poetry <jingguanliuye@gmail.com>
2016-05-01 21:52:46 +0800

    Update 使用多个源文件.c

--- ReadingBookNotes/HeadFirstC/Demo/使用多个源文件.c
+++ ReadingBookNotes/HeadFirstC/Demo/使用多个源文件.c
@@ -1,3 +1,16 @@
+/*
+	1、编译大量文件非常的耗时，可以吧目标代码保存在*.o文件中，加快编译速度
+	2、make工具可以自动化代码的构建过程 make清楚文件之间的依赖关系 可以编译那些修改过的文件 你需要使用makefile告诉make如何构建代码
+	处理makefile的格式要小心 别忘了用tab来缩进 不是空格
+	
+	3、
+	char--是数值（字符编码是一个数字 字符在计算机中以字符编码方式存储）
+	short -- 小数用short
+	long -- 大数用long
+	double-- 高精度浮点数用double
+	float--一般浮点数用float
+*/
+
 #include<stdio.h>
 
 float total = 0.0;
@@ -23,7 +36,7 @@ void main()
 		printf("Price of item:");
 	}
 
-	printf("\nFinal total:%.2\n",total);
+	printf("\nFinal total:%.2f\n",total);
 	printf("Number of items:%hi\n",count);//%i和%d都是表示有符号十进制整数区别在于，%i可以自动将输入的八进制（或者十六进制）转换为十进制，而%d则不会进行转换。
 
 }
