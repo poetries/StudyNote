5110236d8010ab2a47c15fc6d3531c39f58292f9
Poetry <jingguanliuye@gmail.com>
2016-05-05 10:39:01 +0800

    Create test3.java

---
+++ Java/javaDemo/demo03/test3.java
@@ -0,0 +1,13 @@
+//取余
+class test3
+{
+	public static void main(String[] args)
+	{
+		//java中取余运算符的被除数和除数是实数（这与c/c++是不同的）但所得余数的正负只和被除数相同
+
+		double x = 1%-0.3;
+		System.out.println(x);
+		
+	}
+}
+
