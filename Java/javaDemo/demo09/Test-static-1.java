138bb3c41f370204de04bf60fe6478dbddce7d99
Poetry <jingguanliuye@gmail.com>
2016-05-05 23:43:50 +0800

    Update Test-static-1.java

--- Java/javaDemo/demo09/Test-static-1.java
+++ Java/javaDemo/demo09/Test-static-1.java
@@ -2,16 +2,24 @@
 	本程序证明了：
 	static属性i是属于类本身
 	或者说没有对象，我们依然可以直接通过类名的方式访问该类内部的static属性
+	static 方法也是同理
 */
 class A
 {
 	public static int i = 10;
+	
+	public  static void f()
+	{
+		System.out.printf("哈哈哈！！\n");
+	}
 }
 
 class Teststatic1
 {
 	public static void main(String[] args)
 	{
+			System.out.printf("%d\n",A.i);
+			A.f();
 		System.out.printf("%d\n",A.i);
 		
 		
