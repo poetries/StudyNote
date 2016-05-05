7bf1cb8fd8ca0f190d2dd3da630313da66f14ed0
Poetry <jingguanliuye@gmail.com>
2016-05-05 10:25:34 +0800

    Create test1.java

---
+++ Java/javaDemo/demo03/test1.java
@@ -0,0 +1,18 @@
+class test1
+{
+	public static void main(String[] args)
+	{
+		//println把任何的数据类型转换成字符串输出
+		System.out.println(1234);
+		System.out.println("fafa");
+		System.out.println(4254.454);
+		System.out.println('A');
+		System.out.println(453+45);
+		
+		//字符串+可以表示字符串的连接
+		System.out.println('a'+1);
+		System.out.println(""+'a'+1);
+		
+	}
+}
+
