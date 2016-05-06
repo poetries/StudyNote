7d535532fdc405997c213056ecfea5b959a03e1b
Poetry <jingguanliuye@gmail.com>
2016-05-06 08:33:18 +0800

    Update Test_Static3.java

--- Java/javaDemo/demo10/Test_Static3.java
+++ Java/javaDemo/demo10/Test_Static3.java
@@ -1,14 +1,21 @@
 //本程序证明了：一个类的属性可以是类对象
-
 class A
-
-	
+{
+	public void f()
+	{
+		System.out.printf("哈哈哈!\n");
+	}
 }
 
 class B
 {
 	public int i = 10;
-	public A aa = new();
+	public A aa = new A();//aa是一个对象，aa是否也是B的属性？是
+	
+	public void g()
+	{
+		aa.f();
+	}
 	
 }
 
@@ -16,6 +23,7 @@ public class Test_Static3
 {
 	public static void main(String[] args)
 	{
-		
+		B bb = new B();
+		bb.g();
 	}
 }
