9508c30d80653ecd6a6c4781b4d59b6a0da3a30c
Poetry <jingguanliuye@gmail.com>
2016-05-06 09:01:01 +0800

    Update Test_Static2.java

--- Java/javaDemo/demo10/Test_Static2.java
+++ Java/javaDemo/demo10/Test_Static2.java
@@ -9,7 +9,7 @@ class A
 		
 	}
 	
-	public static AgetA() //static不能省略 因为：静态的不可以访问非静态的成员
+	public static A getA() //static不能省略 因为：静态的不可以访问非静态的成员
 	{
 		return aa;
 	}
@@ -22,5 +22,18 @@ public class Test_Static2
 	{
 		// A aa1 = new A(); //如果A类的构造方法是私有的 则new就会报错
 		// A aa2 = new A();
+		
+		A aa1 = A.getA();
+		A aa2 = A.getA();
+		
+		aa1.i = 99;
+		System.out.printf("%d\n",aa2.i); //此步说明aa1 aa2公用一个静态的i成员
+		
+		//检测aa1 aa2是否相等
+		
+		if(aa1 == aa2)
+			System.out.printf("aa1和aa2相等\n");
+		else
+			System.out.printf("aa1和aa2不相等\n");
 	}
 }
