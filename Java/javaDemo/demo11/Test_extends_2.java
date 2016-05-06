7adde5f787ae301ca4365818d71b024abd7de088
Poetry <jingguanliuye@gmail.com>
2016-05-06 10:16:46 +0800

    Update Test_extends_2.java

--- Java/javaDemo/demo11/Test_extends_2.java
+++ Java/javaDemo/demo11/Test_extends_2.java
@@ -1,3 +1,22 @@
+/*
+继承：
+
+1、一个类从已有的类那里获得其的属性和方法，这种现象叫类的继承
+2、这种新类称为子类，也叫 派生类，已有的那个类叫父类，或者基类
+3、继承的好处：
+			代码得到了极大的重用
+			形成一种层次体系结构，为多态创建条件
+
+4、继承实现方式：
+
+				class SubClass extends SuperClass
+				{
+					
+				}
+	
+*/
+
+
 class Human
 {
 	public String  name = "张三";
@@ -21,6 +40,6 @@ public class Test_extends_2
 	public static void main(String[] args)
 	{
 		Graduate gd = new Graduate();
-		System.out.printf("%s %f\n",gd.name,gd.score);
+		System.out.printf("%s %f %s\n",gd.name,gd.score,gd.daoshi);
 	}
 }
