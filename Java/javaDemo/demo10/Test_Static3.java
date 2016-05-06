46ea8c2e8e17d0d491d2ca6c145b626ec32e9645
Poetry <jingguanliuye@gmail.com>
2016-05-06 08:20:53 +0800

    Create Test_Static3.java

---
+++ Java/javaDemo/demo10/Test_Static3.java
@@ -0,0 +1,21 @@
+//本程序证明了：一个类的属性可以是类对象
+
+class A
+
+	
+}
+
+class B
+{
+	public int i = 10;
+	public A aa = new();
+	
+}
+
+public class Test_Static3
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
