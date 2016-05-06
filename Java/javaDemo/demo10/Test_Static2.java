ac4c1922cc952e178cf9cdc18f71c43c1dd23eca
Poetry <jingguanliuye@gmail.com>
2016-05-06 08:45:50 +0800

    Update Test_Static2.java

--- Java/javaDemo/demo10/Test_Static2.java
+++ Java/javaDemo/demo10/Test_Static2.java
@@ -1,13 +1,18 @@
-//本程序让A这个类保证只造出一个对象
+//ѾԌѲɃA֢ٶ`ѣֻ֤լԶһٶהг
 
 class A
 {
 	public int i = 20;
-	A aa = new A();
+	private static A aa = new A(); //aa 是A对象的属性 static不能省 否则getA()方法不能访问aa（静态的不能访问非静态的）
 	
-	private  A()  //private 把new禁止 使之不能造出对象
+	private  A()  //private ёnew޻ֹ ʹ֮һŜլԶהг
 	{
 		
+	}
+	
+	public static AgetA() //static一定不能省略（原因：可以让外部非静态的成员访问静态的方法）
+	{
+		return aa;
 	}
 	
 }
@@ -16,7 +21,7 @@ public class Test_Static2
 {
 	public static void main(String[] args)
 	{
-		// A aa1 = new A();
+		// A aa1 = new A(); //ɧڻA`քٹլ׽רˇ˽Ԑք ղnewߍܡѨխ
 		// A aa2 = new A();
 	}
 }
