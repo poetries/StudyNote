3e15cef6e9c4660b388d74526747ac4748a9162f
Poetry <jingguanliuye@gmail.com>
2016-05-06 08:07:02 +0800

    Create Test_Static1.java

---
+++ Java/javaDemo/demo10/Test_Static1.java
@@ -0,0 +1,38 @@
+class A
+{
+	private int i;
+	private static int cnt = 0;//用静态的少占用空间  用private外部不可修改
+	
+	public A()
+	{
+		++cnt;
+	}
+	
+	public A(int i)
+	{
+		this.i = i;
+		++cnt;
+	}
+	
+	public static int getCnt()
+	{
+		
+		//	return   //返回的 是A对象的个数
+		return cnt;
+	
+	}
+}
+
+public class Test_Static1
+{
+	public static void main(String[] args)
+	{
+		System.out.printf("当前时刻A对象的个数是：%d\n",A.getCnt());
+		
+		A aa1 = new A();
+		System.out.printf("当前时刻A对象的个数是：%d\n",A.getCnt());
+		
+		A aa2 = new A();
+		System.out.printf("当前时刻A对象的个数是：%d\n",A.getCnt());
+	}
+}
