3a2fb1a465862263267c0624055d893b98dd6c9c
Poetry <jingguanliuye@gmail.com>
2016-05-07 20:42:49 +0800

    Create 同包不同类的互相访问.java

---
+++ Java/javaDemo/demo20/同包不同类的互相访问.java
@@ -0,0 +1,25 @@
+//A.java文件
+
+public class A 
+{
+	public void f()
+	{
+		System.out.printf("AAAA");
+	}
+}
+
+//B.java文件
+
+class B
+{
+	public static void main(String[] args)
+	{
+		A aa = new A();
+		aa.f();
+	}
+}
+
+//执行命令 javac A.java B.java   java B 
+
+//附注：因为类A和类B默认是在同一个无名的包中所以彼此可以互相访问
+//只要是丰私有的成员都可以被同包的另一个类访问
