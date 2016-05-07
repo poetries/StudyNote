fefa2c400db8ca178020ba3c1638c8ebf48552d4
Poetry <jingguanliuye@gmail.com>
2016-05-07 14:51:03 +0800

    Create Testinterface_1.java

---
+++ Java/javaDemo/demo17/Testinterface_1.java
@@ -0,0 +1,24 @@
+
+interface It 
+{
+	int i = 20;
+	public void f();//接口方法中不能带主体
+}
+
+abstract class B 
+{
+	public void f()
+	{
+		
+	}
+}
+
+class Testinterface_1
+{
+	public static void main(String[] args)
+	{
+		
+	}
+	
+}
+
