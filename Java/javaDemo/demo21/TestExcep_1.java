c0b2e051f837f1d4b21f2d695cf5d382659f9e9b
Poetry <jingguanliuye@gmail.com>
2016-05-08 00:33:38 +0800

    Update TestExcep_1.java

--- Java/javaDemo/demo21/TestExcep_1.java
+++ Java/javaDemo/demo21/TestExcep_1.java
@@ -4,6 +4,7 @@ class A
 	{
 		int m;
 		m = a/b;
+		//System.out.printf("哈哈哈哈\n");//第6行错 后面就不执行
 		return m;
 	}
 	
@@ -15,17 +16,23 @@ public class TestExcep
 	{
 		A aa = new A();
 		
+		//aa.divide(6,0);
+		
 		//异常一种处理机制 可以用try{} catch来检测估计的那个代码会出异常
 		//异常不是编译时的错误 是程序运行时捕捉到的错误
-		// try
-		// {
-			// aa.divide(6,0);
+		//一个代码里面不能抛出两个异常  
 		
-		// }
-		// catch(ArithmeticException e)//如果try中的代码出错
-		// {
-			// System.out.printf("程序出错了!!!\n");
-		// }
+		try
+		{
+			aa.divide(6,0);
+		
+		}
+		//程序中所有出现的异常都是Exception的子类 不知道代码抛出什么异常就写Exception本身
+		catch(Exception e)//e类似形参 e用来接收出错的信息 如果try中的代码出错
+		{
+			e.printStackTrace();//可以简单理解为输出该异常的具体信息
+			System.out.printf("程序出错了!!!\n");
+		}
 		
 		
 		System.out.printf("今天很开心!!!");
