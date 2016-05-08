fd2b981b88c7772d89406fdbbd1614d7750b2d41
Poetry <jingguanliuye@gmail.com>
2016-05-08 16:22:50 +0800

    Update TestToString_2.java

--- Java/javaDemo/demo22/TestToString_2.java
+++ Java/javaDemo/demo22/TestToString_2.java
@@ -24,3 +24,18 @@ public class TestPoint
 	}
 
 }
+
+/*
+
+	toString 方法总结：
+	
+	1、所有的类都默认自动继承了object类
+	
+	2、object类中的toString方法返回的是类的名字和该对象哈希码组成的一个字符串
+	
+	3、System.out.println(类对象名) 实际输出的是该对象的toString()方法所返回的字符串
+	
+	4、为了实际需要，建议子类重写从父类object继承的toString()方法
+	
+	
+*/
