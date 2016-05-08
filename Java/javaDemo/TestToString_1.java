a5a0b86ee417c137f003bf03ef52ddb0b40d7619
Poetry <jingguanliuye@gmail.com>
2016-05-08 16:01:48 +0800

    Create TestToString_1.java

---
+++ Java/javaDemo/TestToString_1.java
@@ -0,0 +1,17 @@
+class A
+{
+	public String toString()
+	{
+		return "哈哈哈";
+	}
+}
+
+public class TestToString_1
+{
+	public static void main(String[] args)
+	{
+		A aa = new A();
+		System.out.printf("%s\n",aa.toString());
+	}
+
+}
