7d2dece556de4f7d77d4427af7770ef57ab214d4
Poetry <jingguanliuye@gmail.com>
2016-05-10 19:15:00 +0800

    Update TestTextFile_1.java

--- Java/javaDemo/demo32/TestTextFile_1.java
+++ Java/javaDemo/demo32/TestTextFile_1.java
@@ -79,3 +79,81 @@ class MyMonitor implements ActionListener//实现接口的方法 接口是特殊
 		TestTextFile_1.tf3.setText(str3);
 	}
 }	
+
+
+/*********************************************************************************************************************/
+
+
+import java.awt.*;
+import java.awt.event.*; //导入事件包
+
+public class TestTextFile_3
+{
+	public static TextField tf1,tf2,tf3; //属性是静态的
+	
+	public static void main(String[] args)
+	{
+		
+		tf1 = new TextField(30);
+		tf2 = new TextField(30);
+		tf3 = new TextField(30);
+		Button bn = new Button("=");
+		Label Lb = new Label("+");
+		
+		Frame f = new Frame("文本框相加");
+		f.setLayout(new FlowLayout());//Frame 默认布局是BorderLayout 要改成流式布局
+		
+		f.add(tf1);
+		f.add(Lb);
+		f.add(tf2);
+		f.add(bn);
+		f.add(tf3);
+		
+		MyMonitor mm = new MyMonitor();
+		
+		f.addWindowListener(mm);//关闭窗口
+		
+		bn.addActionListener(mm);//监听事件
+		
+		f.pack();
+		f.setVisible(true);//显示
+		
+		
+		
+	}
+}
+
+//关闭窗口
+
+/* class B extends WindowAdapter //e接收aa发送的事件
+{
+	public void  windowClosing(WindowEvent e) //把接口的抽象方法重写
+	{
+		System.exit(-1);
+	}
+} */
+
+//设计监听类
+
+//extends  必须写在implements前面 
+
+class MyMonitor extends WindowAdapter implements ActionListener  //可以实现多个接口  extends WindowAdapter要写在前面
+{
+	public void actionPerformed(ActionEvent e)//方法接收这个事件
+	{
+		int num1 = Integer.parseInt(TestTextFile_3.tf1.getText());
+		int num2 = Integer.parseInt(TestTextFile_3.tf2.getText());
+		
+		int num3 = num1 + num2;
+		TestTextFile_3.tf3.setText(num3+"");//转换为字符串
+		
+	
+	}
+	
+	//把接口的抽象方法重写
+	public void  windowClosing(WindowEvent e) 
+	{
+		System.exit(-1);
+	}
+	
+}	
