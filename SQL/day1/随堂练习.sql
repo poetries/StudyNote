213737009a85acf0eb26bad4d1eb1c0ef6075864
Poetry <jingguanliuye@gmail.com>
2016-05-15 10:05:44 +0800

    Update 随堂练习.sql

--- SQL/day1/随堂练习.sql
+++ SQL/day1/随堂练习.sql
@@ -6,11 +6,12 @@ create table dept
 )
 create table emp
 ( 
+	-- 主键约束
 	emp_id int constraint pk_emp_id_hahah primary key,
 	emp_name nvarchar(20) not null,
 	emp_sex nchar(1),
 	dept_id int constraint fk_dept_id_heihei foreign key references dept(dept_id)
-	-- 最后一个字段逗号可写可不写 最好不写 但是oracle必须不能写
+	-- 外键约束  最后一个字段逗号可写可不写 最好不写 但是oracle必须不能写
 )
 
 create table student
